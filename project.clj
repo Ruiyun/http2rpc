a698d157d7e15a20fd889a0d864d3c802bef08f6
Ruiyun Wen <ruiyun.wen@gmail.com>
2014-12-04 21:29:25 +0800

    fix the uberwar problem

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
 (defproject http2rpc "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.16.5"]
+                 [metosin/compojure-api "0.16.5" :exclusions [org.clojure/java.classpath
+                                                              slingshot
+                                                              commons-codec
+                                                              clj-time]]
                  [metosin/ring-http-response "0.5.2"]
-                 [metosin/ring-swagger-ui "2.0.17"]]
+                 [metosin/ring-swagger-ui "2.0.17"]
+                 [clj-time "0.6.0"]
+                 [com.fasterxml.jackson.core/jackson-core "2.3.2"]]
   :ring {:handler http2rpc.handler/app}
-  :uberjar-name "server.jar"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.8.13"]]}})
+                   :plugins [[lein-ring "0.8.13" :exclusions [org.clojure/clojure]]]}})
--- src/http2rpc/handler.clj
+++ src/http2rpc/handler.clj
@@ -8,16 +8,14 @@
 (defapi app
   {:formats [:json-kw]}
   (swagger-ui)
-  (swagger-docs
-    :title "http2rpc")
-  (swaggered "invoke"
-    :description "Invoke PBRPC Platform Service"
-    (POST* "/Http2RpcService/Invoke" []
-      :return Message
-      :body-params [host :- s/Str
-                    port :- s/Int
-                    service :- s/Str
-                    method :- s/Str
-                    params :- s/Any]
-      :summary "调用指定的pbrpc服务接口，并获取调用返回值"
-      (ok {:response (str "invoke, " method ", param, " (class params))}))))
+  (swagger-docs :title "http2rpc")
+  (swaggered "invoke" :description "Invoke PBRPC Platform Service"
+             (POST* "/Http2RpcService/Invoke" []
+                    :return Message
+                    :body-params [host :- s/Str
+                                  port :- s/Int
+                                  service :- s/Str
+                                  method :- s/Str
+                                  params :- s/Any]
+                    :summary "调用指定的pbrpc服务接口，并获取调用返回值"
+                    (ok {:response (str "invoke, " method ", param, " params " param's type:" (type params))}))))
