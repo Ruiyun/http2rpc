4379d079f9438d039057c7a2df919f6f9ac9ab42
Ruiyun Wen <ruiyun.wen@gmail.com>
2014-12-09 15:42:09 +0800

    work in progress

--- src/http2rpc/handler.clj
+++ src/http2rpc/handler.clj
@@ -11,25 +11,24 @@
   {:formats [:json-kw]}
   (swagger-ui)
   (swagger-docs :title "http2rpc")
-  (swaggered "invoke" :description "Invoke PBRPC Platform Service (invalid)"
-             (POST* "/Http2RpcService/Invoke" []
-                    :return Response
-                    :body-params [host :- s/Str
-                                  port :- s/Int
-                                  service :- s/Str
-                                  method :- s/Str
-                                  params :- s/Any]
-                    :summary "调用指定的pbrpc服务接口，并获取调用返回值"
-                    (ok {:response (str "invoke, " method ", param, " params " param's type:" (type params))})))
+  ;; (swaggered "invoke" :description "Invoke PBRPC Platform Service (invalid)"
+  ;;            (POST* "/Http2RpcService/Invoke" []
+  ;;                   :return Response
+  ;;                   :body-params [host :- s/Str
+  ;;                                 port :- s/Int
+  ;;                                 service :- s/Str
+  ;;                                 method :- s/Str
+  ;;                                 params :- s/Any]
+  ;;                   :summary "调用指定的pbrpc服务接口，并获取调用返回值"
+  ;;                   (ok {:response (str "invoke, " method ", param, " params " param's type:" (type params))})))
   (swaggered "invoke echo" :description "Invoke the standard Echo Service"
              (POST* "/Http2RpcService/InvokeEcho" []
                     :return Message
                     :body-params [host :- s/Str
-                                  port :- s/Int
+                                  {port :- s/Int 8031}
                                   message :- s/Str]
                     :summary "调用新建平台默认的EchoService，用于验证新建平台是否正常工作"
-                    (let [response (try
-                                     (invoke-echo host port message)
-                                     (catch Exception ex
-                                       ex))]
-                      (ok {:message (.getMessage response)})))))
+                    (try
+                      (ok {:message (.. (invoke-echo host port message) (getMessage))})
+                      (catch Exception ex
+                        (internal-server-error (str ex)))))))
